460334842314a10ad7db75683006439c12e115c0
jarabik8810 <alexjarabik@gmail.com>
2023-08-18 10:47:32 +0200

    uploading files

---
+++ frontend/src/pages/Logout.js
@@ -0,0 +1,8 @@
+import { redirect } from "react-router";
+
+export function action() {
+    localStorage.removeItem('token');
+    localStorage.removeItem('expiration');
+
+    return redirect('/');
+}
